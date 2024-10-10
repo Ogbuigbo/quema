c41cf45818bbc8631201514241246f0ab9b28ffb
Ogbuigbo Chijioke <chijiokejames82@gmail.com>
2024-10-10 21:37:03 +0100

    first commit

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -6,19 +6,22 @@ function Hero() {
       <div
         className="absolute inset-0 bg-cover bg-no-repeat z-3"
         style={{
-          backgroundImage: "url('https://tinypic.host/images/2024/10/09/DALLE-2024-10-09-00.34.17---A-professional-HR-consulting-environment-featuring-a-diverse-group-of-people-in-a-modern-office-space-collaborating-and-discussing-projects-around-a.webp')", 
+          backgroundImage:
+            "url('https://tinypic.host/images/2024/10/09/DALLE-2024-10-09-00.34.17---A-professional-HR-consulting-environment-featuring-a-diverse-group-of-people-in-a-modern-office-space-collaborating-and-discussing-projects-around-a.webp')",
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
         }}
       />
       {/* Overlay */}
       <div className="absolute inset-0 bg-[#1c2833] opacity-80 z-1"></div>
-      
+
       <div className="relative mx-auto max-w-screen-xl px-4 md:py-32 py-20 lg:flex lg:items-center z-3 md:pb-[200px]">
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl text-white">
             Elevate Your Workforce.
-            <strong className="font-extrabold text-[#4a90e2] sm:block"> Drive Business Results. </strong>
+            <strong className="font-extrabold text-[#4a90e2] sm:block">
+              Drive Business Results.
+            </strong>
           </h1>
 
           <p className="mt-4 sm:text-xl/relaxed text-white">
@@ -27,14 +30,14 @@ function Hero() {
 
           <div className="mt-8 flex flex-wrap justify-center gap-4">
             <a
-              className="block w-full rounded bg-[#4a90e2] px-12 py-3 text-sm font-medium text-white shadow hover:bg-[#3b7cc4] focus:outline-none focus:ring active:bg-[#356bb1] sm:w-auto"
+              className="block rounded bg-[#4a90e2] px-8 py-3 text-sm font-medium text-white shadow hover:bg-[#3b7cc4] focus:outline-none focus:ring active:bg-[#356bb1] sm:w-auto"
               href="#"
             >
               Get Started
             </a>
 
             <a
-              className="block w-full rounded px-12 py-3 text-sm font-medium border text-[#fff] shadow hover:text-[#16202a] focus:outline-none focus:ring active:text-[#0f151b] sm:w-auto"
+              className="block rounded px-8 py-3 text-sm font-medium border text-[#fff] shadow hover:text-[#16202a] focus:outline-none focus:ring active:text-[#0f151b] sm:w-auto"
               href="#"
             >
               Learn More
@@ -47,8 +50,3 @@ function Hero() {
 }
 
 export default Hero;
-
-
-
-
-
